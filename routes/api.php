a0c771cb293badfb79d51980a662ac9f46ddbd2d
saif saif <saefmziri10@gmail.com>
2026-07-28 14:41:06 +0300

    ..

--- routes/api.php
+++ routes/api.php
@@ -23,17 +23,17 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::post('register', [AuthController::class, 'register']);
         Route::apiResource('users', UserController::class)->only(['index', 'show']);
         Route::patch('users/{id}/status', [UserController::class, 'changeStatus']);
-        Route::apiResource('Projects', ProjectController::class)->only(['store','update', 'destroy']);
-        Route::apiResource('Tasks', TaskController::class)->only(['store','update', 'destroy']);
+        Route::apiResource('projects', ProjectController::class)->only(['store','update', 'destroy']);
+        Route::apiResource('tasks', TaskController::class)->only(['store','update', 'destroy']);
     });
 
     Route::middleware('CheckUser:Admin,Employee')->group(function () {
-        Route::apiResource('Projects', ProjectController::class)->only(['index','show']);
-        Route::apiResource('Tasks', TaskController::class)->only(['index','show']);
+        Route::apiResource('projects', ProjectController::class)->only(['index','show']);
+        Route::apiResource('tasks', TaskController::class)->only(['index','show']);
     });
 
     Route::middleware('CheckUser:Employee')->group(function(){
-        Route::patch('Tasks/{id}/Status', [TaskController::class,'changeStatus']);
+        Route::patch('tasks/{id}/status', [TaskController::class,'changeStatus']);
     });
 
     Route::middleware('IsOwnerOrAdmin')->group(function () {
